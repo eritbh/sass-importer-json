a1d633eb410d6d92a8cecc2ef73fceefdbadaebf
Erin <github@eritbh.me>
2025-02-23 15:50:10 -0500

    add some basic test cases

---
+++ test/basic_paths/index.js
@@ -0,0 +1,21 @@
+import assert from 'node:assert';
+import {dirname, join} from 'node:path';
+import test from 'node:test';
+import {compile} from 'sass';
+import jsonImporter from '../../dist/index.js';
+const dir = dirname(new URL(import.meta.url).pathname);
+
+test('basic paths are resolved and values are pulled in', t => {
+	const {css} = compile(join(dir, 'test.scss'), {
+		importers: [jsonImporter()],
+	});
+
+	assert.strictEqual(css, `
+	:root {
+	  --a: 1;
+	  --b: asdf asdf asdf;
+	  --c: [multiple, things];
+	  --d: bar;
+	}
+	`.trim().replace(/^\t+/gm, ''));
+});
---
+++ test/import_vs_use/index.js
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+import {dirname, join} from 'node:path';
+import test from 'node:test';
+import {compile, Logger} from 'sass';
+import jsonImporter from '../../dist/index.js';
+const dir = dirname(new URL(import.meta.url).pathname);
+
+test('usage from @import succeeds', t => {
+	const {css} = compile(join(dir, 'import.scss'), {
+		importers: [jsonImporter()],
+		logger: Logger.silent,
+	});
+
+	assert.strictEqual(css.trim(), `
+	:root {
+	  --a: 1;
+	  --b: 2;
+	  --c: 3;
+	  --d: 4;
+	}
+	`.trim().replace(/^\t+/gm, ''));
+});
+
+test('not loaded via @use', t => {
+	Array(4).map((_, i) => {
+		assert.throws(() => {
+			compile(join(dir, `use${i+1}.scss`));
+		});
+	})
+})
